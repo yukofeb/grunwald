d0edd8e980a1a03b2d92d86af75b102a11ba9032
yukofeb <yuko.february@gmail.com>
2015-07-09 00:06:03 +0200

    add config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :domain => 'smtp.gmail.com',
+    :authentication => 'plain',
+    :user_name => ENV["MAILER_USER"],
+    :password => ENV["MAILER_PASSWORD"]
+  }
 end
