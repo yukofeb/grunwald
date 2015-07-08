7710f6d803659cd236064f699a0df70740b55c11
yukofeb <yuko.february@gmail.com>
2015-07-08 10:03:16 +0200

    support bootstrap, rspec

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def bread
+  end
+
+  def cafe
+  end
+
+  def shop
+  end
+end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  subject { page }
+
+  describe "Home" do
+    before { visit root_path }
+    it { should have_title('Grunwald') }
+  end
+
+  describe "Bread Making Class" do
+    before { visit bread_path }
+    it { should have_title('Grunwald | Bread Making Class') }
+  end
+
+  describe "Cafe" do
+    before { visit cafe_path }
+    it { should have_title('Grunwald | Cafe') }
+  end
+
+  describe "Shop" do
+    before { visit shop_path }
+    it { should have_title('Grunwald | Shop') }
+  end
+end
