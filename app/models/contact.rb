9a33c64832a5dff233cb7ec04429f995d35fe957
yukofeb <yuko.february@gmail.com>
2015-07-12 23:26:16 +0200

    add test

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,8 +3,11 @@ class Contact
 
   attr_accessor :name, :email, :content
 
-  validates(:name, :presence => true)
-  validates(:email, :presence => true,
-                    :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i })
-  validates(:content, :presence => true)
+  validates :name, :presence => true,
+                   :length => { maximum: 30 }
+  validates :email, :presence => true,
+                    :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i },
+                    :length => { maximum: 100 }
+  validates :content, :presence => true,
+                      :length => { maximum: 3000 }
 end
