67d3ac6e168a2d52da0912b3ccb0e9b7a63b28cb
yukofeb <yuko.february@gmail.com>
2015-09-23 19:51:57 -0400

    add_form_contents

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,19 @@
 class Contact
   include ActiveModel::Model
 
-  attr_accessor :name, :email, :content,
-                :home_tel, :mobile_tel,
-                :zip, :address
+  attr_accessor :name, :pc_email, :mobile_email,
+                :content, :home_tel, :mobile_tel,
+                :zip, :address, :bread_making_course
 
   validates :name, :presence => true,
                    :length => { maximum: 30 }
-  validates :email, :presence => true,
-                    :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i },
-                    :length => { maximum: 100 }
+  validates :pc_email, :presence => true,
+                       :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i },
+                       :length => { maximum: 100 }
+  validates :mobile_email, :presence => false,
+                       :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i },
+                       :length => { maximum: 100 },
+                        unless: Proc.new { |a| a.mobile_email.blank? }
   validates :content, :presence => true,
                       :length => { maximum: 3000 }
   validates :home_tel, :presence => false,
@@ -26,4 +30,5 @@ class Contact
                    unless: Proc.new { |a| a.zip.blank? }
   validates :address, :presence => false,
                       :length => { maximum: 300 }
+  validates :bread_making_course, :presence => false
 end
