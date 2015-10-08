2ba8751662b3f9e810380ce2443a585b22d08de3
yukofeb <yuko.february@gmail.com>
2015-10-08 17:23:31 -0400

    modify

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,7 +15,6 @@ class StaticPagesController < ApplicationController
 
   def search_menus
     @cafemenu_url = params[:cafemenu_url]
-    @cafemenu_url.sub!(/http/, 'https')
     render
   end
 end
